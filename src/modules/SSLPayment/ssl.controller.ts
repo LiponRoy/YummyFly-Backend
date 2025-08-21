7b8b0d536b7d6a53fc330218a53c45cfeb116be6
lipon <liponbdbd@gmail.com>
2025-08-21 17:24:01 +0600

    created failed status of SSL payment

--- src/modules/SSLPayment/ssl.controller.ts
+++ src/modules/SSLPayment/ssl.controller.ts
@@ -74,8 +74,8 @@ export const PaymentOrder = catchAsync(async (req: Request, res: Response) => {
     currency: "BDT",
     tran_id: tran_id,
     success_url: `http://localhost:4000/api/v1/orderPayment/success/${tran_id}`,
-    fail_url: `http://localhost:4000/api/v1/orderPayment/fail/${tran_id}`,
-    cancel_url: `http://localhost:4000/api/v1/orderPayment/cancel/${tran_id}`,
+    fail_url: `http://localhost:4000/api/v1/orderPayment/failed/${tran_id}`,
+    cancel_url: `http://localhost:4000/api/v1/orderPayment/canceled/${tran_id}`,
     ipn_url: `http://localhost:4000/api/v1/orderPayment/ipn/${tran_id}`,
     shipping_method: "Courier",
     product_name: "group of product",
@@ -133,6 +133,24 @@ export const PaymentOrderSuccess = catchAsync(async (req: Request, res: Response
 
   if (result.modifiedCount > 0) {
     return res.redirect(`http://localhost:3000/paymentSuccess/${transId}`);
+  } else {
+    return res.status(404).json({
+      message: "Transaction not found or already updated",
+      transId,
+    });
+  }
+});
+
+export const PaymentOrderFailed = catchAsync(async (req: Request, res: Response) => {
+  const { transId } = req.params;
+
+  const result = await Order.updateOne(
+    { tran_id: transId },
+    { $set: { paymentStatus: "FAILED" } }
+  );
+
+  if (result.modifiedCount > 0) {
+    return res.redirect(`http://localhost:3000/paymentFailed/${transId}`);
   } else {
     return res.status(404).json({
       message: "Transaction not found or already updated",
--- src/modules/SSLPayment/ssl.route.ts
+++ src/modules/SSLPayment/ssl.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { PaymentOrder,PaymentOrderSuccess } from "./ssl.controller";
+import { PaymentOrder,PaymentOrderFailed,PaymentOrderSuccess } from "./ssl.controller";
 
 
 const router = Router();
 
 router.post("/orderPayment/init", PaymentOrder);
 router.post("/orderPayment/success/:transId", PaymentOrderSuccess);
-
+router.post("/orderPayment/failed/:transId", PaymentOrderFailed);
+// router.post("/orderPayment/canceled/:transId", PaymentOrderCanceled);
 
 
 export const orderRoutes = router;
