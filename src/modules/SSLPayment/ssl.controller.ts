df021638ef8539eb3e90da8f987e6903c1024f34
LiponRoy <liponbdbd@gmail.com>
2025-08-25 07:26:07 +0600

    created vercel.json to deploy on vercel

--- src/modules/SSLPayment/ssl.controller.ts
+++ src/modules/SSLPayment/ssl.controller.ts
@@ -1,103 +1,42 @@
-import { Request, Response } from "express";
-import catchAsync from "../../utils/catchAsync";
-import { sslcz } from "../../utils/sslcommerz";
-import { Order } from "./ssl.order.model";
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import { sslcz } from '../../utils/sslcommerz';
+import { Order } from './ssl.order.model';
 
-//   const { amount, cus_name, cus_email, total_product, total_price } = req.body;
-//   const tran_id = "ORDER_" + Date.now(); // unique transaction ID
-    
-
-//     const data = {
-//       total_amount: amount,
-//       currency: "BDT",
-//       tran_id: tran_id,
-//       success_url: `http://localhost:4000/orderPayment/success/${tran_id}`,
-//       fail_url: "http://localhost:3030/fail",
-//       cancel_url: "http://localhost:3030/cancel",
-//       ipn_url: "http://localhost:3030/ipn",
-//       shipping_method: "Courier",
-//       product_name: "group of product",
-//       product_category: "group of product category",
-//       product_profile: "general",
-//       cus_name: cus_name,
-//       cus_email: cus_email,
-//       cus_add1: "Dhaka",
-//       cus_add2: "Dhaka",
-//       cus_city: "Dhaka",
-//       cus_state: "Dhaka",
-//       cus_postcode: "1000",
-//       cus_country: "Bangladesh",
-//       cus_phone: "01711111111",
-//       cus_fax: "01711111111",
-//       ship_name: cus_name,
-//       ship_add1: "Dhaka",
-//       ship_add2: "Dhaka",
-//       ship_city: "Dhaka",
-//       ship_state: "Dhaka",
-//       ship_postcode: "1000",
-//       ship_country: "Bangladesh",
-//     };
-
-//     const apiResponse = await sslcz.init(data);
-//     const GatewayPageURL = apiResponse.GatewayPageURL;
-
-//     if (GatewayPageURL) {
-//       console.log("Redirecting to:", GatewayPageURL);
-//       // return res.json({ url: GatewayPageURL }); // frontend will redirect
-
-//           // Save new order
-//     const order = await Order.create({
-//       tran_id,
-//       amount,
-//       currency: "BDT",
-//       cus_name,
-//       cus_email,
-//       total_product,
-//       total_price,
-//       paymentStatus: "INITIATED",
-//     });
-
-//    res.json({ url: GatewayPageURL }); // frontend will redirect
-
-//     } else {
-//       return res.status(400).json({ message: "Payment gateway init failed" });
-//     }
- 
-// });
 export const PaymentOrder = catchAsync(async (req: Request, res: Response) => {
   const { amount, cus_name, cus_email, total_product, total_price } = req.body;
   // unique transaction ID
-  const tran_id = "ORDER_" + Date.now(); 
+  const tran_id = 'ORDER_' + Date.now();
 
   const data = {
     total_amount: amount,
-    currency: "BDT",
+    currency: 'BDT',
     tran_id: tran_id,
     success_url: `http://localhost:4000/api/v1/orderPayment/success/${tran_id}`,
     fail_url: `http://localhost:4000/api/v1/orderPayment/failed/${tran_id}`,
     cancel_url: `http://localhost:4000/api/v1/orderPayment/canceled/${tran_id}`,
     ipn_url: `http://localhost:4000/api/v1/orderPayment/ipn/${tran_id}`,
-    shipping_method: "Courier",
-    product_name: "group of product",
-    product_category: "group of product category",
-    product_profile: "general",
+    shipping_method: 'Courier',
+    product_name: 'group of product',
+    product_category: 'group of product category',
+    product_profile: 'general',
     cus_name,
     cus_email,
-    cus_add1: "Dhaka",
-    cus_add2: "Dhaka",
-    cus_city: "Dhaka",
-    cus_state: "Dhaka",
-    cus_postcode: "1000",
-    cus_country: "Bangladesh",
-    cus_phone: "01711111111",
-    cus_fax: "01711111111",
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: '01711111111',
+    cus_fax: '01711111111',
     ship_name: cus_name,
-    ship_add1: "Dhaka",
-    ship_add2: "Dhaka",
-    ship_city: "Dhaka",
-    ship_state: "Dhaka",
-    ship_postcode: "1000",
-    ship_country: "Bangladesh",
+    ship_add1: 'Dhaka',
+    ship_add2: 'Dhaka',
+    ship_city: 'Dhaka',
+    ship_state: 'Dhaka',
+    ship_postcode: '1000',
+    ship_country: 'Bangladesh',
   };
 
   const apiResponse = await sslcz.init(data);
@@ -108,55 +47,56 @@ export const PaymentOrder = catchAsync(async (req: Request, res: Response) => {
     await Order.create({
       tran_id,
       amount,
-      currency: "BDT",
+      currency: 'BDT',
       cus_name,
       cus_email,
       total_product,
       total_price,
-      paymentStatus: "INITIATED",
+      paymentStatus: 'INITIATED',
     });
 
     return res.json({ url: GatewayPageURL }); // frontend will redirect
   } else {
-    return res.status(400).json({ message: "Payment gateway init failed." });
+    return res.status(400).json({ message: 'Payment gateway init failed.' });
   }
 });
 
+export const PaymentOrderSuccess = catchAsync(
+  async (req: Request, res: Response) => {
+    const { transId } = req.params;
 
-export const PaymentOrderSuccess = catchAsync(async (req: Request, res: Response) => {
-  const { transId } = req.params;
+    const result = await Order.updateOne(
+      { tran_id: transId },
+      { $set: { paymentStatus: 'SUCCESS' } },
+    );
 
-  const result = await Order.updateOne(
-    { tran_id: transId },
-    { $set: { paymentStatus: "SUCCESS" } }
-  );
+    if (result.modifiedCount > 0) {
+      return res.redirect(`http://localhost:3000/paymentSuccess/${transId}`);
+    } else {
+      return res.status(404).json({
+        message: 'Transaction not found or already updated',
+        transId,
+      });
+    }
+  },
+);
 
-  if (result.modifiedCount > 0) {
-    return res.redirect(`http://localhost:3000/paymentSuccess/${transId}`);
-  } else {
-    return res.status(404).json({
-      message: "Transaction not found or already updated",
-      transId,
-    });
-  }
-});
+export const PaymentOrderFailed = catchAsync(
+  async (req: Request, res: Response) => {
+    const { transId } = req.params;
 
-export const PaymentOrderFailed = catchAsync(async (req: Request, res: Response) => {
-  const { transId } = req.params;
+    const result = await Order.updateOne(
+      { tran_id: transId },
+      { $set: { paymentStatus: 'FAILED' } },
+    );
 
-  const result = await Order.updateOne(
-    { tran_id: transId },
-    { $set: { paymentStatus: "FAILED" } }
-  );
-
-  if (result.modifiedCount > 0) {
-    return res.redirect(`http://localhost:3000/paymentFailed/${transId}`);
-  } else {
-    return res.status(404).json({
-      message: "Transaction not found or already updated",
-      transId,
-    });
-  }
-});
-
-
+    if (result.modifiedCount > 0) {
+      return res.redirect(`http://localhost:3000/paymentFailed/${transId}`);
+    } else {
+      return res.status(404).json({
+        message: 'Transaction not found or already updated',
+        transId,
+      });
+    }
+  },
+);
